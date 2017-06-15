0455b2b50a1a9837d0a20ab5a8409b24e27634a8
Alex Dulub <alex.dulub@designsters.com>
2017-06-15 17:13:15 +0300

    Changed autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -14,13 +14,13 @@ which autoreconf >/dev/null || \
   (echo "configuration failed, please install autoconf first" && exit 1)
 autoreconf --install --force --warnings=all
 
-cd src/cpp-ethereum
-git submodule update --init
-./scripts/install_deps.sh
-cmake .
+cd src/solidity 
+cmake -DSTATIC_LINKING=TRUE
 make -j `nproc`
 
 cd ..
-cd solidity
+cd cpp-ethereum
+git submodule update --init
+./scripts/install_deps.sh
 cmake .
 make -j `nproc`
