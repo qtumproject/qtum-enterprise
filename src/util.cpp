528464731f577d77b503317f625e8782d83ad706
zhengyi <zhengyi.bupt@qq.com>
2018-08-12 09:56:15 +0800

    set a special dir for beta

--- src/util.cpp
+++ src/util.cpp
@@ -563,7 +563,7 @@ fs::path GetDefaultDataDir()
     // Unix: ~/.qtum
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "QtumX";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "QtumXBeta";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -573,10 +573,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/QtumX";
+    return pathRet / "Library/Application Support/QtumXBeta";
 #else
     // Unix
-    return pathRet / ".qtumx";
+    return pathRet / ".qtumxbeta";
 #endif
 #endif
 }
